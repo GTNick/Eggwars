409fe2f9448278ad45001073dd01d2b25ed8ca23
GTNick <nickpp1111@yahoo.com>
2016-07-11 21:42:39 -0400

    Create Gen.php

---
+++ Gen.php
@@ -0,0 +1,12 @@
+namespace CrazierDevotee0\Gen
+
+use pocketmine/plugin/PluginBase
+
+class Gen extends PluginBase {
+
+public function onEnable("Eggwars Gen Plugin is now enabled") {
+
+}
+public function onDisable("Eggwars Gen Plugin is now disabled") {
+
+}
