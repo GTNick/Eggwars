cd263594786e6006880464a96ebca27c987ffdd6
GTNick <nickpp1111@yahoo.com>
2016-07-13 19:58:25 -0400

    Update and rename Gen.php to scr/Egg.php

---
+++ scr/Egg.php
@@ -0,0 +1,22 @@
+namespace CrazierDevotee0\Egg;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\event\block\BlockBreakEvent;
+
+class Egg extends PluginBase {
+
+public function onEnable("Eggwars Egg Plugin is now enabled") {
+
+}
+plublic function onBreak(glass $g) {
+if($this->plugin->
+
+
+
+public function onDisable("Eggwars Egg Plugin is now disabled") {
+
+}
